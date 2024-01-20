97d4b6989287685609884361adc42ffaeb29640e
Byron Serrano <bsantyserrano@gmail.com>
2024-01-20 00:24:09 -0500

    Added Cloud DB

--- src/main/java/com/example/mongodb/api/v1/PaisController.java
+++ src/main/java/com/example/mongodb/api/v1/PaisController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1.0/pais")
+@RequestMapping("/api/pais")
 public class PaisController {
 
 	@Autowired
